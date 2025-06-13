c79c9c601aa8ecdacd8076a166294fa0cf62d897
NTKP <tankerping@gmail.com>
2025-06-13 18:18:35 +0800

    Completed first model training, but encounter some issues in accuracy, where the training time series is hourly based, but accidently used as daily based

--- model_training.py
+++ model_training.py
@@ -4,7 +4,21 @@ import numpy as np
 from sklearn.preprocessing import MinMaxScaler
 import numpy as np
 from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import LSTM, Dense, Input
+from tensorflow.keras.layers import LSTM, Dense, Input, Dropout
+from joblib import dump, load
+from sklearn.metrics import mean_squared_error, r2_score
+
+# Function to extract the data into an array of sequences. Eg: (x = row 0-19 if n_steps=20, y = row 20)
+def create_sequences(input_data, n_steps, n_ahead=1):
+    X, y = [], []
+    for i in range(len(input_data) - n_steps - n_ahead + 1):
+        end_ix = i + n_steps
+        seq_x = input_data[i:end_ix, :]  # All columns
+        seq_y = input_data[end_ix, :]   # All columns
+        X.append(seq_x)
+        y.append(seq_y)
+    return np.array(X), np.array(y)
+
 
 # import data
 df = pd.read_csv('Data/cleaned_klangWLRF.csv', parse_dates=["datetime"], index_col="datetime")
@@ -32,76 +46,117 @@ print(wlT.isna().sum())
 print(wlT.isna().sum().sum())
 print(wlT[wlT.isna().any(axis=1)])
 
-
 # scare the data within the range of 0 to 1, and convert the data to numpy array
 scaler = MinMaxScaler()
 combined_data = pd.concat([rfT, wlT], axis=1)
 scaled_data = scaler.fit_transform(combined_data)
-
+dump(scaler, 'scaler.save')
 
 # split 70% data for training, 20% for validate, 10% for testing
 total_samples = len(scaled_data)
 train_end = int(total_samples * 0.7)
-validation_end = int(total_samples * 0.9)
 train_data = scaled_data[:train_end]
-validation_data = scaled_data[train_end:validation_end]
-test_data = scaled_data[validation_end:]
+validation_data = scaled_data[train_end:]
+print(f"Validation rows: {validation_data.shape[0]}")
 
 
 # convert the numpy arrays back to df
 columns = [rfT.columns.tolist() + wlT.columns.tolist()]
 train_df = pd.DataFrame(train_data, columns=columns)
 validation_df = pd.DataFrame(validation_data, columns=columns)
-test_df = pd.DataFrame(test_data, columns=columns)
 
 # check for the data stats
 stats_train = train_df.describe().loc[['mean', 'std', 'min', 'max']]
 stats_validation = validation_df.describe().loc[['mean', 'std', 'min', 'max']]
-stats_test = test_df.describe().loc[['mean', 'std', 'min', 'max']]
 print("Training Data Stats:\n", stats_train, "\n")
 print("Validation Data Stats:\n", stats_validation, "\n")
-print("Testing Data Stats:\n", stats_test, "\n")
 
-# Function to extract the data into an array of sequences. Eg: (x = row 0-19 if n_steps=20, y = row 20)
-def create_sequences(input_data, n_steps, n_ahead=1):
-    X, y = [], []
-    for i in range(len(input_data) - n_steps - n_ahead + 1):
-        end_ix = i + n_steps
-        seq_x = input_data[i:end_ix, :]  # All columns
-        seq_y = input_data[end_ix, 10:20]   # Target WL, one step ahead, columns 11-20 (10-19 in 0-indexed)
-        X.append(seq_x)
-        y.append(seq_y)
-    return np.array(X), np.array(y)
 
 # create training data with the sequences function
-X_train, y_train = create_sequences(train_data, n_steps=20)
-X_validation, y_validation = create_sequences(validation_data, n_steps=20)
-X_test, y_test = create_sequences(test_data, n_steps=20)
+X_train, y_train = create_sequences(train_data, n_steps=365)
+X_validation, y_validation = create_sequences(validation_data, n_steps=365)
 print("Training data shape:", X_train.shape, y_train.shape)
+print("Validation data shape:", X_validation.shape, y_validation.shape)
+
+def training():
+    # Build the LSTM model
+    model = Sequential([
+        Input(shape=(365, 20)),
+        LSTM(128, return_sequences=True),     # Return full sequence for next LSTM
+        Dropout(0.2),
+        LSTM(64, return_sequences=False),     # Return final output only
+        Dropout(0.2),
+        Dense(20)    
+    ])
+
+    # Optional to include a second LSTM layer
+    # model = Sequential([
+    #     LSTM(64, return_sequences=True, input_shape=(20, 20)),
+    #     LSTM(32),
+    #     Dense(32, activation='relu'),
+    #     Dense(10)
+    # ])
+
+    model.compile(optimizer='adam', loss='mse')
 
 
-# Build the LSTM model
-model = Sequential([
-    Input(shape=(20, 20)),
-    LSTM(64, activation='relu'),  # '20' represents the number of features in each time step
-    Dense(10)
-])
+    # Train the model & save the model and training history
+    history = model.fit(
+        X_train, y_train,
+        epochs=50,
+        batch_size=32,
+        validation_data=(X_validation, y_validation)
+    )
+    dump(model, 'Model/wl_model.keras')
+    dump(history, 'Model/model_history.joblib')
 
-# Optional to include a second LSTM layer
-# model = Sequential([
-#     LSTM(64, return_sequences=True, input_shape=(20, 20)),
-#     LSTM(32),
-#     Dense(32, activation='relu'),
-#     Dense(10)
-# ])
+def evaluation():
+    # Load the model and history
+    model = load('Model/wl_model.keras')
+    history = load('Model/model_history.joblib')
 
-model.compile(optimizer='adam', loss='mse')
+    # Plot the training and validation loss
+    plt.figure(figsize=(10, 6))
+    plt.plot(history.history['loss'], label='Training Loss')
+    plt.plot(history.history['val_loss'], label='Validation Loss')
+    plt.title('Model Training History')
+    plt.xlabel('Epoch')
+    plt.ylabel('Loss')
+    plt.legend()
+    plt.grid(True)
+    plt.show()
+
+    train_pred = model.predict(X_train)
+    validation_pred = model.predict(X_validation)
+    print("MSE:", mean_squared_error(y_validation, validation_pred))
+    print("R² Score:", r2_score(y_validation, validation_pred))
+    plot_predictions_with_error(y_train, train_pred, 'Training Data: True vs Predicted')
+    plot_predictions_with_error(y_validation, validation_pred, 'Validation Data: True vs Predicted')
+
+def plot_predictions_with_error(true_data, predicted_data, title):
+    plt.figure(figsize=(10, 12))
+    
+    # Flatten arrays to ensure compatibility with matplotlib
+    true_data_flatten = true_data.flatten()
+    predicted_data_flatten = predicted_data.flatten()
+    
+    # Calculate error
+    error = true_data_flatten - predicted_data_flatten
+    positive_error_std = np.std(error[error >= 0])
+    negative_error_std = np.std(error[error < 0])
+    
+    # Plot true data vs. predicted data
+    ax1 = plt.subplot(2, 1, 1)  # 2 rows, 1 column, 1st subplot
+    ax1.plot(true_data_flatten, label='True Data')
+    ax1.plot(predicted_data_flatten, label='Predicted Data', alpha=0.7)
+    ax1.set_title(title)
+    ax1.set_ylabel('Value')
+    ax1.legend()
+    plt.show()
 
 
-# Train the model
-history = model.fit(
-    X_train, y_train,
-    epochs=50,
-    batch_size=32,
-    validation_data=(X_validation, y_validation)
-)
+
+if __name__ == "__main__":
+    #training()
+    evaluation()
+    
---
+++ prediction.py
@@ -0,0 +1,65 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+from sklearn.preprocessing import MinMaxScaler
+import numpy as np
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import LSTM, Dense, Input
+from joblib import dump, load
+
+def create_sequences(input_data, n_steps=365):
+    input_data = np.array(input_data)
+    
+    if len(input_data) >= n_steps:
+        seq_x = input_data[-n_steps:, :]
+        return np.array([seq_x])
+    else:
+        raise ValueError(f"Input data has fewer rows ({len(input_data)}) than n_steps ({n_steps})")
+
+model = load('Model/wl_model.keras')
+
+# import data
+df = pd.read_csv('Data/cleaned_klangWLRF.csv', parse_dates=["datetime"], index_col="datetime")
+rfT = df.reset_index().pivot_table(index='datetime', columns='station_id', values='rf')
+rfT = rfT.sort_index() 
+wlT = df.reset_index().pivot_table(index='datetime', columns='station_id', values='wl')
+wlT = wlT.sort_index() 
+rfT = rfT.add_prefix('rf_')
+wlT = wlT.add_prefix('wl_')
+# clean data with mode values
+for col in rfT.columns:
+    mode = rfT[col].mode()
+    if not mode.empty:
+        rfT[col].fillna(mode[0], inplace=True)
+for col in wlT.columns:
+    mode = wlT[col].mode()
+    if not mode.empty:
+        wlT[col].fillna(mode[0], inplace=True)
+# scare the data within the range of 0 to 1, and convert the data to numpy array
+scaler = load('scaler.save')
+combined_data = pd.concat([rfT, wlT], axis=1)
+scaled_data = scaler.transform(combined_data)
+# convert the numpy arrays back to df
+columns = [rfT.columns.tolist() + wlT.columns.tolist()]
+input_df = pd.DataFrame(scaled_data, columns=columns)
+
+# Define the ppredicted df to insert the predicted values
+predicted_df = input_df.tail(365)
+
+for i in range(365):  # Predict one year ahead
+    # Define the input data for prediction
+    input_data = create_sequences(predicted_df, n_steps=365)
+
+    # Predict the values using the model
+    predicted_value = model.predict(input_data)
+    temp_df = pd.DataFrame(predicted_value, columns=columns)
+    predicted_df = pd.concat([predicted_df, temp_df], axis=0)
+
+
+reversed_data = scaler.inverse_transform(predicted_df)
+reversed_df = pd.DataFrame(reversed_data, columns=columns)
+reversed_df = reversed_df.iloc[365:]
+print(reversed_df)
+# Save the predicted values to a CSV file
+reversed_df.to_csv('Data/predicted_klangWLRF.csv', index=False)
+
